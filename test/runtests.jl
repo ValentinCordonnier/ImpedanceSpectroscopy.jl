ce7b3fa0ba8d4a0802b5b403d45025b10bea5e14
Valentin Cordonnier <71752917+Valeuhhh@users.noreply.github.com>
2022-07-19 15:54:11 +0200

    Add files via upload

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,12 @@ using ImpedanceSpectroscopy
 using Test
 
 @testset "ImpedanceSpectroscopy.jl" begin
-    # Write your tests here.
+
+    R1 = R(10,1000)
+	C2 = C(0.000006,1000)
+	L3 = L(0.00005,1000)
+	CPE1 = CPE([0.00005,3],1000)
+
+	circuit1 = BaseCircuit([R1,C2,L3],["R1-C2-L3"])
+	circuit2 = BaseCircuit([R1,C2,[R1,C2,R1],L3],["R1-C2-[R1-C2-R1]-L3"])
 end
